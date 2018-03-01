84c6c90fedd5cdfe96d974a1ca83bdf276c72381
Luke Joshua <20785216@sun.ac.za>
2018-03-01 20:40:18 +0200

    read file

---
+++ luke/main.js
@@ -0,0 +1,5 @@
+const readFile = require('./readFile')
+
+let exampleObj = readFile('../input/a_example.in')
+
+console.log(exampleObj)
---
+++ luke/readFile.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+module.exports = function readFile(fileName){
+	let fileString = fs.readFileSync(fileName).toString()
+	let lines = fileString.split('\n')
+	let [rows, columns,numVehicles,numRides,bonus, steps] = lines[0].split(' ').map(x => Number(x))
+	let rides = []
+	for(let line of lines.slice(1,numRides+1)){
+		let lineArr = line.split(' ').map(x => Number(x))
+		rides.push({
+			from: [lineArr[0], lineArr[1]],
+			to: [lineArr[2], lineArr[3]],
+			start : lineArr[4],
+			end: lineArr[5]
+		})
+	}
+
+	return {
+		rows,
+		columns,
+		numVehicles,
+		bonus,
+		steps,
+		rides
+	}
+}
